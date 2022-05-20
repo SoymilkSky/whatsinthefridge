61ccdbc82bca11c41a2356095a7e47f2ec788842
DannyWong <danny.96.wong@gmail.com>
2022-05-19 21:26:44 -0700

    feat: loginscreen + homescreen work started feat: integrate firebase auth for login and register

---
+++ screens/loginScreen/LoginScreen.tsx
@@ -0,0 +1,141 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  KeyboardAvoidingView,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { useNavigation } from '@react-navigation/core';
+import { auth } from "../../firebase";
+
+function LoginScreen() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        navigation.navigate('HomeScreen');
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleSignup = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials: { user: any; }) => {
+        const user = userCredentials.user;
+        console.log('Registered with: ', user.email);
+      })
+      .catch((error: any) => {
+        alert(error);
+      });
+  };
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials: { user: any; }) => {
+        const user = userCredentials.user;
+        console.log('Logged in with: ', user.email);
+      })
+      .catch((error: any) => {
+        alert(error);
+      });
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Text style={styles.name}>What's in your fridge?</Text>
+      <View style={styles.inputContainer}>
+        <StatusBar style="auto" />
+        <TextInput
+          placeholder="E-mail Address"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+        />
+        <Text>{email}</Text>
+        <Text>{password}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignup}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  name: {
+    fontSize: 24,
+    padding: 10,
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
